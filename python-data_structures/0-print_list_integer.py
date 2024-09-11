7bd5c3c75b10b0b9212e91b93338b51a3c029a5d
intex <jerrymeletiche123@gmail.com>
2024-09-11 18:10:12 -0400

    printea 5 espacios

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
- for num in int_list:
-  print(num)
 
-  integers 
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
