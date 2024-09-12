b7e05a08606bd1b14e4b628d826f1cca0196e991
intex <jerrymeletiche123@gmail.com>
2024-09-12 18:56:59 -0400

    mm

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_n = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_d[roman_string[j]] - 2 * \
-                        roman_d[roman_string[j - 1]]
+
+    # Define Roman numeral values
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 
+        'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    # Traverse the string from right to left
+    for char in reversed(roman_string):
+        current_value = roman_values.get(char, 0)
+        
+        # If the current value is smaller than the previous one, subtract it (special case)
+        if current_value < prev_value:
+            total -= current_value
         else:
-            roman_n += roman_d[roman_string[j]]
-    return roman_n
+            total += current_value
+        
+        prev_value = current_value
+    
+    return total
