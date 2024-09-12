bdf100bccebac032051c2eab8926b5435051acf4
intex <jerrymeletiche123@gmail.com>
2024-09-12 18:02:32 -0400

    mm

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    # Get the keys of the dictionary and sort them
-  
-
     sorted_keys = sorted(a_dictionary.keys())
-   
-   
     for key in sorted_keys:
         print(f"{key}: {a_dictionary[key]}")
-
-
-
 sample_dict = {
     "banana": 3,
     "apple": 5,
