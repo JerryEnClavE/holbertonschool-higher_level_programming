e2a2f88acaa24dbe079bf115252d7241d9041901
intex <jerrymeletiche123@gmail.com>
2024-09-12 20:05:55 -0400

    mm

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num_of_elements = 0
-    for index in range(x):
-        try:
-            print(my_list[index], end="")
-            num_of_elements += 1
-        except IndexError:
-            break
-
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=' ')
+            count += 1
+    except IndexError:
+        pass
     print()
-    return num_of_elements
+    return count
