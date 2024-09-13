ba642c5b8c7292536224a6f6bf949288208b08bf
intex <jerrymeletiche123@gmail.com>
2024-09-12 22:53:03 -0400

    mm

--- python-test_driven_development/0-add_integer.py, tests/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py, tests/0-add_integer.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+    Insert here module comment
+"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, int) and not isinstance(a, float):
+    """ add two integers or flots """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
+    if type(a) is float:
         a = int(a)
-    if isinstance(b, float):
+    if type(b) is float:
         b = int(b)
     return (a + b)
