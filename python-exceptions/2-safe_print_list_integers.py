d2a05da76159cd6f45941a2660fbdb78bbcdbf68
intex <jerrymeletiche123@gmail.com>
2024-09-12 20:32:03 -0400

    mm

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     printed_count = 0
     for i in range(x):
         try:
-            # Try to format and print the element as an integer
             print("{:d}".format(my_list[i]), end=" ")
             printed_count += 1
         except (ValueError, TypeError, IndexError):
-            # Ignore non-integers and out-of-bounds index silently
             pass
-    print()  # To ensure the output ends with a newline
+    print()
     return printed_count
