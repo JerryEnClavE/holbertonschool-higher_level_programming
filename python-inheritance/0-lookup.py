f726ff963a8c7eeb7131906474a3f37701303fa3
intex <jerrymeletiche123@gmail.com>
2024-09-24 11:28:20 -0400

    ....

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "lookup"
 
+
 def lookup(obj):
-  "returns a list of the attributes and methods of any object"
-  return dir(obj)
+    "returns a list of the attributes and methods of any object"
+    return dir(obj)
