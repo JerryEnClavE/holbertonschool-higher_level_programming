97c029281d798455d5a432e43856f9265a710c29
intex <jerrymeletiche123@gmail.com>
2024-09-24 12:07:53 -0400

    >>>

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"pritn list"
+"""Inherits from list and adds a method to print the list in sorted order."""
+
 
 
 class MyList(list):
     def print_sorted(self):
-        "Sort the list in ascending order and print it"
+        """Prints the list in ascending sorted order without modifying the original list."""
         print(sorted(self))
