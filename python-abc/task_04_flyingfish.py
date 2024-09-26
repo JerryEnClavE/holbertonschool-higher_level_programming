4347df05dad1e1b673bfa1ee6120a410e8bc9b85
intex <jerrymeletiche123@gmail.com>
2024-09-26 11:37:36 -0400

    >>>

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -34,13 +34,3 @@ flying_fish = FlyingFish()
 flying_fish.swim()
 flying_fish.fly()
 flying_fish.habitat()
-
-print(FlyingFish.mro())
-
-
-from task_04_flyingfish import Fish, FlyingFish
-
-flying_fish = FlyingFish()
-flying_fish.swim()
-flying_fish.fly()
-flying_fish.habitat()
