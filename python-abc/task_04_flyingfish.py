99e6004b42b3f44073e7c57d09cd0f3f528eaf63
intex <jerrymeletiche123@gmail.com>
2024-09-26 11:16:59 -0400

    >>>

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
+
 class Fish:
+
     def swim(self):
         print("The fish is swimming")
+
     def habitat(self):
         print("The fish live in the water")
 
+
 class Bird:
+
     def fly(self):
-        print("The bird is fliying")
+        print("The bird is flying")
+
     def habitat(self):
         print("The bird live in the sky")
 
-#FlyingFish class inheriting from both Fish and Bird
+
 class FlyingFish(Fish, Bird):
+
     def fly(self):
         print("The flying fish is soaring!")
 
     def swim(self):
         print("The flying fish is swimming")
-    
+
     def habitat(self):
         print("The flying fish lives both in water and the sky!")
