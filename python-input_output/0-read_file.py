c1170ff31a4a059574145c31082b33d20cd18197
intex <jerrymeletiche123@gmail.com>
2024-09-30 14:07:37 -0400

    >>>

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 
 
 def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
         print(content, end="")
