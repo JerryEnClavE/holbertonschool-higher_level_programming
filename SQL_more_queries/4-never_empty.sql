2daf3fc8379c2467d983506eae17750a047eb067
intex <jerrymeletiche123@gmail.com>
2024-10-24 16:03:23 -0400

    >>>

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,4 @@
--- creates the table force_name on your MySQL server
+-- Ensures that the table creation does not fail if the table already exists.
+--Defines the id column as an integer, sets a default value of 1, and ensures it cannot be NULL.
+--Defines the name column as a variable-length string
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
