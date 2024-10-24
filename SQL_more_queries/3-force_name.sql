bcfd20f046ffda1558dfe6708e916f424daf8880
intex <jerrymeletiche123@gmail.com>
2024-10-24 03:34:16 -0400

    >>>

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Active: 1729751780644@@localhost@3306
+-- creates the table force_name on your MySQL server
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
