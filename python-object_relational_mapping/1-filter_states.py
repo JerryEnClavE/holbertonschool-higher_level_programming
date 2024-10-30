6d8be4900ad6d4111746483f6da5f1ca7b68272b
intex <jerrymeletiche123@gmail.com>
2024-10-30 11:49:21 -0400

    >>>

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
+"""Lists states"""
+
+
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    #get MySQL username, pasword, and database name from arguments
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
 
-    # Connect to MySQL server on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306, 
-                         user=mysql_user, password=mysql_password, 
-                         db=db_name)
-    
-    #create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    #Execute yhe query to select states starting with 'N'
-    query = "SELECT * FROM states WHERE names LIKE 'N%' ORDER BY ASC;"
-    cursor.execute(query)
-    
-    #Fetch all results and print them
-    states = cursor.fechall()
-    for state in states:
-        print(states)
-
-#Close cursor and database connection
-cursor.close()
-db.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDERS BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswith("N"):
+            print(row)
+        cur.close()
+        conn.close()
