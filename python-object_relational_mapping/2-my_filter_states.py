d302cd1ec344368d053bcb0834a8492a2768895a
intex <jerrymeletiche123@gmail.com>
2024-10-30 13:52:48 -0400

    >>>

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,16 +10,13 @@ if __name__ == "__main__":
                                passwd=argv[2], db=argv[3], charset="utf8")
         cur = conn.cursor()
 
-        # Usar parámetros en lugar de formatear la consulta directamente
         query = """
     SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC"""
         cur.execute(query, (argv[4],))
 
         query_rows = cur.fetchall()
         for row in query_rows:
-            # Imprimir el id y el nombre del estado de forma clara
             print(f"{row[0]}: {row[1]}")
-            # Suponiendo que el id es el primer campo y el nombre el segundo
 
         cur.close()
     except MySQLdb.Error as e:
