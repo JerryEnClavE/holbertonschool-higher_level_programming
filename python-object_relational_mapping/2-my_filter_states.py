8ea9de3a6926a37f3c52d69321c732bfd39189b2
intex <jerrymeletiche123@gmail.com>
2024-10-30 13:43:13 -0400

    >>>

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,15 +5,24 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    query = """
-SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
-    query = query.format(argv[4])
-    cur.execute(query)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3], charset="utf8")
+        cur = conn.cursor()
+        
+        # Usar parámetros en lugar de formatear la consulta directamente
+        query = """
+        SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC"""
+        cur.execute(query, (argv[4],))
+        
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            # Imprimir el id y el nombre del estado de forma clara
+            print(f"{row[0]}: {row[1]}")  # Suponiendo que el id es el primer campo y el nombre el segundo
+        
+        cur.close()
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        if conn:
+            conn.close()
