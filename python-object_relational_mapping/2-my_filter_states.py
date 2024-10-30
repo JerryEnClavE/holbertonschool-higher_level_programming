ba3b78ca164c7d90c8392836a5be5c61dca39383
intex <jerrymeletiche123@gmail.com>
2024-10-30 13:39:07 -0400

    >>>

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,27 +5,15 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) != 5:
-        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name> <state_name>")
-        exit(1)
-
-    try:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                               passwd=argv[2], db=argv[3], charset="utf8")
-        cur = conn.cursor()
-        
-        query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC"
-        cur.execute(query, (argv[4],))
-        
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-    
-    except MySQLdb.Error as e:
-        print(f"Error {e.args[0]}: {e.args[1]}")
-    
-    finally:
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
