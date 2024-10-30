7243b4279c409285aec50c941fa5d64106d41064
intex <jerrymeletiche123@gmail.com>
2024-10-30 13:28:32 -0400

    >>>

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
+"""Lists states"""
+
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Recopilar argumentos de línea de comandos
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Conectar a la base de datos MySQL
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Crear un objeto cursor
-    cursor = db.cursor()
-
-    # Crear la consulta SQL
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Obtener todos los resultados
-    results = cursor.fetchall()
-
-    # Imprimir los resultados en el formato requerido
-    for row in results:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    # Cerrar el cursor y la conexión
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
