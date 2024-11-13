c6466407dea8ad9244b6a242e021e06823592a73
intex <jerrymeletiche123@gmail.com>
2024-11-13 19:27:14 -0400

    >>>

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -10,12 +10,12 @@ def home():
     return render_template('index.html')
 
 
-@app.route('about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 
-@app.route('contact')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
